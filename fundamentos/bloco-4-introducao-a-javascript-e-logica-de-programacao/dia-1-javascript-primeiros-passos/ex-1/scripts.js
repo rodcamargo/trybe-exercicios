52682cce85e59af4731f319bac9d97edbee94190
Rodrigo Camargo <rodrigocamargo.dev@gmail.com>
2022-03-07 18:41:28 -0300

    Movendo ex. aula pra pasta aula e add exercicios

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex-1/scripts.js
@@ -0,0 +1,8 @@
+const a = 5
+const b = 3
+
+console.log ('Adição ' + (a + b));
+console.log ('Subtração ' + (a - b));
+console.log ('Multiplicação ' + (a * b));
+console.log ('Divisão ' + (a / b));
+console.log ('Modulo ' + (a % b));
